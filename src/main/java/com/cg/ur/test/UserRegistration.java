23a9dee6d34ab48d94d4e906a378269247299a27
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-29 22:11:11 +0530

    [Siddhartha] modified: added condition for last name

--- src/main/java/com/cg/ur/test/UserRegistration.java
+++ src/main/java/com/cg/ur/test/UserRegistration.java
@@ -5,26 +5,45 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class UserRegistration {
-	
-	public void firstNameValidator(String firstName) {
+
+	Scanner in = new Scanner(System.in);
+
+	public void firstNameValidator() {
+		System.out.println("Enter your first name :");
+		System.out.println("First letter should be capital and shuold have min 3 characters");
+		String firstName = in.next();
 		Pattern pattern = Pattern.compile("^([A-Z])[a-z]{2,}$");
 		Matcher matcher = pattern.matcher(firstName);
 		Boolean b = matcher.find();
-        if(b)
-        	System.out.println("First Name is Validated");
-        else
-        	System.out.println("Please enter the name as mentioned");
-        
+		if (b)
+			System.out.println("First Name is Validated");
+		else
+			System.out.println("Please enter the First name as mentioned");
+
+	}
+
+	public void lastNameValidator() {
+		System.out.println("Enter your last name :");
+		System.out.println("First letter should be capital and shuold have min 3 characters");
+		String lastName = in.next();
+		Pattern pattern = Pattern.compile("^([A-Z])[a-z]{2,}$");
+		Matcher matcher = pattern.matcher(lastName);
+		Boolean b = matcher.find();
+		if (b)
+			System.out.println("Last Name is Validated");
+		else
+			System.out.println("Please enter the Last name as mentioned");
+
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Page");
-        System.out.println("Enter your first name :");
-        System.out.println("First letter should be capital and shuold have min 3 characters");
-        Scanner in = new Scanner(System.in);
-        UserRegistration newUser = new UserRegistration();
-        newUser.firstNameValidator(in.next());
-        in.close();
+		UserRegistration newUser = new UserRegistration();
+		// for first name
+		newUser.firstNameValidator();
+		// for last name
+		newUser.lastNameValidator();
+
 	}
 
 }
