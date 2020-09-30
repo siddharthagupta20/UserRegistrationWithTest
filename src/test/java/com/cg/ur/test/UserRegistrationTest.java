fa3f3fadd25111acd576da801305e950d56fdf8d
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-10-01 01:15:09 +0530

    [Siddhartha] modified: handling exceptions in test cases

--- src/test/java/com/cg/ur/test/UserRegistrationTest.java
+++ src/test/java/com/cg/ur/test/UserRegistrationTest.java
@@ -8,32 +8,56 @@ public class UserRegistrationTest {
 	@Test
 	public void givenFirstName_whenValid_shouldReturnValid() {
 		UserRegistration ur = new UserRegistration();
-		assertTrue("Valid", ur.firstNameValidator("John"));
+		try{
+			assertTrue("Valid", ur.firstNameValidator("John"));
+		}
+		catch(FirstNameValidatorException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Test
 	public void givenLastName_whenValid_shouldReturnValid() {
 		UserRegistration ur = new UserRegistration();
-		assertTrue("Valid", ur.lastNameValidator("Doe"));
+		try{
+			assertTrue("Valid", ur.lastNameValidator("Doe"));
+		}
+		catch(LastNameValidatorException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void givenEmail_whenValid_shouldReturnValid() {
 		UserRegistration ur = new UserRegistration();
-		assertTrue("Valid", ur.emailValidator("johndoe-20@gmail.com"));
+		try{
+			assertTrue("Valid", ur.emailValidator("johndoe-20@gmail.com"));
+		}
+		catch(EmailValidatorException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void givenMobile_whenValid_shouldReturnValid() {
 		UserRegistration ur = new UserRegistration();
-		assertTrue("Valid", ur.mobileNoValidator("11 1111111111"));
+		try{
+			assertTrue("Valid", ur.mobileNoValidator("11 1111111111"));
+		}catch(MobileNoValidatorException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void givenPassword_whenValid_shouldReturnValid() {
 		UserRegistration ur = new UserRegistration();
-		assertTrue("Valid", ur.passwordValidator("Johndoecool#1"));
+		try{
+			assertTrue("Valid", ur.passwordValidator("Johndoecool#1"));
+		}
+		catch(PasswordValidatorException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
